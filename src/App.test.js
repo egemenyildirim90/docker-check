b285c041ea64d9f772d0e7e1e5e7ca3af3d4a4b4
egemenyildirim90 <egemenyildirim1@gmail.com>
2023-11-06 18:33:19 +0300

    new version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText("Ask your question");
+  expect(titleElement).toBeInTheDocument();
 });
+
