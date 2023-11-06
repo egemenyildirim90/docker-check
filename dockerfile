6d3f1ef82f3d3746a73af0475618da3afdb53e3f
egemenyildirim90 <egemenyildirim1@gmail.com>
2023-11-06 18:21:09 +0300

    update

--- dockerfile
+++ dockerfile
@@ -3,6 +3,7 @@ WORKDIR '/app'
 COPY package.json .
 RUN npm install
 COPY . .
+EXPOSE 8080
 RUN npm run build
 
 FROM nginx
--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [answers, setAnswers] = useState([]);
+
+  const handleQuestionSubmit = () => {
+    // Add the question to the answers list
+    setAnswers([...answers, question]);
+
+    // Clear the input field
+    setQuestion('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          it worked 2!!! <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1 className="text-center">Ask your question</h1>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Ask"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            />
+            <div className="input-group-append">
+              <button className="btn btn-primary" onClick={handleQuestionSubmit}>
+                Send
+              </button>
+            </div>
+          </div>
+          {answers.length > 0 && (
+            <div>
+              <h2 className="text-center">Answers</h2>
+              <ul>
+                {answers.map((answer, index) => (
+                  <li key={index}>{answer}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
